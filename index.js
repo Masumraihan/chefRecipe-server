80382108377b215974ef402132c7304ef475ffc6
Masumraihan <mdmasumraihan1@gamil.com>
2023-05-02 00:38:21 +0600

    vercel node js configuration added and bannerData fixed img link

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
-//const bannerData = require("./data/bannerData.json");
+const bannerData = require("./data/bannerData.json");
 
 app.use(cors());
 
@@ -11,6 +11,9 @@ app.get("/", (req, res) => {
   res.send("hello world");
 });
 
+app.get("/bannerData",(req,res) => {
+    res.send(bannerData)
+})
 
 app.listen(port, () => {
   console.log(`chef recipe is running port ${port}`);
