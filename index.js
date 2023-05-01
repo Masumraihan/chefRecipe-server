bc396f0bffd9764c48580f38a9a8c47c6743c187
Masumraihan <mdmasumraihan1@gamil.com>
2023-05-02 00:25:27 +0600

    server is created

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+//const bannerData = require("./data/bannerData.json");
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+
+app.listen(port, () => {
+  console.log(`chef recipe is running port ${port}`);
+});
